955f0aa9619ba9e984cfb8f9d1519b68a17081a9
Alex <alekstok97@mail.ru>
2022-09-13 17:56:30 +0600

    first Task

---
+++ src/component/ProfileSideBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, TextField } from "@mui/material";
+
+export default function ProfileSideBar() {
+  return (
+    <div>
+      <Box component="form" noValidate color="default">
+        <TextField label="test" color="primary"></TextField>
+      </Box>
+    </div>
+  );
+}
